72357c240841fb9b624f965328c4d7487b4bc35c
faded_Black <inkeetcha234@gmail.com>
2024-11-20 16:29:46 +0530

    added user names

--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -50,9 +50,9 @@ function printDice5() {
 
 function printDice6() {
   let box = '┏━━━━━━━━┓\n';
-  box += '┃⬤  ⬤  ⬤ ┃\n';
-  box += '┃        ┃\n';
-  box += '┃⬤  ⬤  ⬤ ┃\n';
+  box += '┃ ⬤   ⬤  ┃ \n';
+  box += '┃ ⬤   ⬤  ┃ \n';
+  box += '┃ ⬤   ⬤  ┃ \n';
   box += '┗━━━━━━━━┛';
 
   return box;
@@ -70,50 +70,27 @@ function printDice(diceNumber) {
 }
 
 function dice() {
-  let diceValue = Math.random() * 6;
-  diceValue = Math.ceil(diceValue);
-
-  return diceValue;
+  return Math.ceil(Math.random() * 6);
 }
 
-function snakeOrLadder(currentPlayerPos) {
-  switch (currentPlayerPos) {
-    case 5: return 58;
-    case 14: return 59;
-    case 38: return 20;
-    case 42: return 60;
-    case 45: return 7;
-    case 51: return 10;
-    case 53: return 72;
-    case 64: return 83;
-    case 65: return 54;
-    case 75: return 94;
-    case 91: return 73;
-    case 97: return 3;
-    default: return currentPlayerPos;
+function getALine(length) {
+  let line = '';
+  for (let iterator = 0; iterator < length; iterator += 1) {
+    line += '━';
   }
+
+  return line;
 }
 
 function createMessageBox(message) {
-  let box = '┏';
-  for (let index = 0; index < message.length; index += 1) {
-    box += '━';
-  }
-
-  box += '┓\n┃' + message + '┃\n┗';
-
-  for (let index = 0; index < message.length; index += 1) {
-    box += '━';
-  }
-  box += '┛\n';
-
-  return box;
+  const box = '┏' + getALine(message.length) + '┓\n┃' + message + '┃\n┗';
+  return box + getALine(message.length) + '┛\n';
 }
 
 function rollDice() {
   let diceValue = 0;
 
-  for (let noOfTimes = 0; noOfTimes < 500; noOfTimes += 20) {
+  for (let noOfTimes = 0; noOfTimes < 350; noOfTimes += 20) {
     for (let counter = 0; counter < noOfTimes * 1000000; counter += 1) {
     }
 
@@ -126,26 +103,48 @@ function rollDice() {
   return diceValue;
 }
 
-function playerPosMessage(updatedPlayerPos, currentPlayerPos, diceValue) {
-  if (updatedPlayerPos < currentPlayerPos) {
+function getSnakeOrLadder(playerPosition) {
+  switch (playerPosition) {
+    case 5: return 58;
+    case 14: return 59;
+    case 38: return 20;
+    case 42: return 60;
+    case 45: return 7;
+    case 51: return 10;
+    case 53: return 72;
+    case 64: return 83;
+    case 65: return 54;
+    case 75: return 94;
+    case 91: return 73;
+    case 97: return 3;
+    default: return playerPosition;
+  }
+}
+
+function playerPosMessage(updatedPlayerPos, playerPosition, diceValue) {
+  if (updatedPlayerPos < playerPosition) {
     console.log(createMessageBox("Ohh noo 😱.... 🐍 A snake bite you.."));
   }
-  if (updatedPlayerPos > currentPlayerPos) {
+  if (updatedPlayerPos > playerPosition) {
     console.log(createMessageBox("Hurray 🙌.... 🪜 You climbed a ladder.."));
   }
-  if (updatedPlayerPos === currentPlayerPos) {
+  if (updatedPlayerPos === playerPosition) {
     console.log(createMessageBox("Moved Forward by " + diceValue + "...😊"));
   }
 }
 
-function updatePosition(currentPlayerPos, playerNumber) {
+function updatePosition(updatedPlayerPos, playerPosition, diceValue, playerNumber) {
+  playerPosMessage(updatedPlayerPos, playerPosition, diceValue);
+  console.log(playerNumber + " Current Position is: " + updatedPlayerPos);
+}
+
+function getNewPosition(playerPosition, playerNumber) {
   const diceValue = rollDice();
 
-  currentPlayerPos += currentPlayerPos + diceValue <= 100 ? diceValue : 0;
-  const updatedPlayerPos = snakeOrLadder(currentPlayerPos);
+  playerPosition += (playerPosition + diceValue <= 100) ? diceValue : 0;
+  const updatedPlayerPos = getSnakeOrLadder(playerPosition);
 
-  playerPosMessage(updatedPlayerPos, currentPlayerPos, diceValue);
-  console.log(playerNumber + " Current Position is: " + updatedPlayerPos);
+  updatePosition(updatedPlayerPos, playerPosition, diceValue, playerNumber);
 
   return updatedPlayerPos;
 }
@@ -154,43 +153,53 @@ function didPlayerWin(p1Position, p2Position, p3Position, p4Position) {
   return p1Position === 100 || p2Position === 100 || p3Position === 100 || p4Position === 100;
 }
 
-function whoWon(p1Position, p2Position, p3Position, p4Position) {
-  if (p1Position === 100) { return '🥳 Player 1 Won.. 🎊\n'; }
-  if (p2Position === 100) { return '🥳 Player 2 Won.. 🎊\n'; }
-  if (p3Position === 100) { return '🥳 Player 3 Won.. 🎊\n'; }
-  if (p4Position === 100) { return '🥳 Player 4 Won.. 🎊\n'; }
+function whoWon(p1Position, p2Position, p3Position, p4Position, playerNames) {
+  if (p1Position === 100) { return '🥳' + playerNames[0] + 'Won.. 🎊\n'; }
+  if (p2Position === 100) { return '🥳' + playerNames[1] + 'Won.. 🎊\n'; }
+  if (p3Position === 100) { return '🥳' + playerNames[2] + 'Won.. 🎊\n'; }
+  if (p4Position === 100) { return '🥳' + playerNames[3] + 'Won.. 🎊\n'; }
+}
+
+function getPlayerNames(noOfPlayer) {
+  let playerNames = [];
+  for (let player = 0; player < noOfPlayer; player += 1) {
+    playerNames[player] = prompt("Enter name of Player: ");
+  }
+
+  return playerNames;
 }
 
 function startPlay() {
-  let p1Position = 1;
-  let p2Position = 1;
-  let p3Position = 1;
-  let p4Position = 1;
+  let p1Position = 0;
+  let p2Position = 0;
+  let p3Position = 0;
+  let p4Position = 0;
 
   console.log(createMessageBox("Snake 🐍 And Ladder 🪜 "));
 
   const noOfPlayer = +prompt("Enter No Of Players : ", '1');
-  let value = 1;
+  const playerNames = getPlayerNames(noOfPlayer);
 
   while (!didPlayerWin(p1Position, p2Position, p3Position, p4Position)) {
-    prompt("Player " + value + "'s turn");
+    let turn = 1;
+    prompt(playerNames[turn - 1] + "'s turn");
 
-    if (value === 1) {
-      p1Position = updatePosition(p1Position, "Player 1");
+    if (turn === 1) {
+      p1Position = getNewPosition(p1Position + 1, playerNames[turn - 1]);
     }
-    if (value === 2) {
-      p2Position = updatePosition(p2Position, "Player 2");
+    if (turn === 2) {
+      p2Position = getNewPosition(p2Position + 1, playerNames[turn - 1]);
     }
-    if (value === 3) {
-      p3Position = updatePosition(p3Position, "Player 3");
+    if (turn === 3) {
+      p3Position = getNewPosition(p3Position + 1, playerNames[turn - 1]);
     }
-    if (value === 4) {
-      p4Position = updatePosition(p4Position, "Player 4");
+    if (turn === 4) {
+      p4Position = getNewPosition(p4Position + 1, playerNames[turn - 1]);
     }
-    value = value === noOfPlayer ? 1 : value + 1;
+    turn = turn === noOfPlayer ? 1 : turn + 1;
   }
 
-  return whoWon(p1Position, p2Position, p3Position, p4Position);
+  return whoWon(p1Position, p2Position, p3Position, p4Position, playerNames);
 }
 
 console.log(startPlay());
