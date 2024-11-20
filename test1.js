00828912f542891e6f576a2eac25fb5c5241251d
faded_Black <inkeetcha234@gmail.com>
2024-11-20 11:03:16 +0530

    added symbols for dice

--- test1.js
+++ test1.js
@@ -1,47 +1,81 @@
-function createBox(p1, p2, p3, p4) {
-  let box = '┏━━━━━━━┓\n';
-  box += '┃ ' + p1 + ' ' + p2 + ' ┃\n';
-  box += '┃ ' + p4 + ' ' + p4 + ' ┃\n';
-  box += '┗━━━━━━━┛\n';
+function printDice1() {
+  let box = '┏━━━━━━━━┓\n';
+  box += '┃        ┃\n';
+  box += '┃   ⬤    ┃\n';
+  box += '┃        ┃\n';
+  box += '┗━━━━━━━━┛\n';
 
   return box;
 }
 
-function createHeader() {
-  let heading = '';
-  for (let index = 0; index < 8; index += 1) {
-    heading += '━━━━┳━━━━';
+function printDice2() {
+  let box = '┏━━━━━━━━┓\n';
+  box += '┃ ⬤      ┃\n';
+  box += '┃        ┃\n';
+  box += '┃     ⬤  ┃\n';
+  box += '┗━━━━━━━━┛\n';
+
+  return box;
+}
+
+function printDice3() {
+  let box = '┏━━━━━━━━┓\n';
+  box += '┃⬤       ┃\n';
+  box += '┃   ⬤    ┃\n';
+  box += '┃      ⬤ ┃\n';
+  box += '┗━━━━━━━━┛\n';
+
+  return box;
+}
+
+function printDice4() {
+  let box = '┏━━━━━━━━┓\n';
+  box += '┃ ⬤   ⬤  ┃\n';
+  box += '┃        ┃\n';
+  box += '┃ ⬤   ⬤  ┃\n';
+  box += '┗━━━━━━━━┛\n';
+
+  return box;
+}
+
+function printDice5() {
+  let box = '┏━━━━━━━━┓\n';
+  box += '┃ ⬤   ⬤  ┃\n';
+  box += '┃   ⬤    ┃\n';
+  box += '┃ ⬤   ⬤  ┃\n';
+  box += '┗━━━━━━━━┛\n';
+
+  return box;
+}
+
+function printDice6() {
+  let box = '┏━━━━━━━━┓\n';
+  box += '┃⬤  ⬤  ⬤ ┃\n';
+  box += '┃        ┃\n';
+  box += '┃⬤  ⬤  ⬤ ┃\n';
+  box += '┗━━━━━━━━┛\n';
+
+  return box;
+}
+
+function printDice(diceNumber) {
+  switch (diceNumber) {
+    case 1: return printDice1();
+    case 2: return printDice2();
+    case 3: return printDice3();
+    case 4: return printDice4();
+    case 5: return printDice5();
+    case 6: return printDice6();
   }
-  return '┏━━━' + heading + '━━━┓';
 }
 
-function createBody(p1, p2, p3, p4) {
-  let body = '';
-  for (let index = 0; index < 4; index += 1) {
-    body += '    ┃        ┃    ';
-    body += '    ┃        ┃    ';
-  }
+function dice() {
+  let diceValue = Math.random() * 6;
+  diceValue = Math.ceil(diceValue);
 
-  return '┣   ' + body + '   ┫';
+  return diceValue;
 }
 
-function createFooter() {
-  let footing = '';
-  for (let index = 0; index < 8; index += 1) {
-    footing += '━━━━┻━━━━';
-  }
-  return '┗━━━' + footing + '━━━┛';
-
-}
-
-
-console.log(createHeader());
-for (let index = 0; index < 10; index += 1){
-  console.log(createBody('p1', 'p2', 'p3', 'p4'));
-}
-console.log(createFooter());
-
-
-
-
+console.log(printDice(dice()));
+// console.log(printDice5());
 
